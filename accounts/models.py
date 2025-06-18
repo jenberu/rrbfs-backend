4670de78e4ad37391cc2189761a78b566cb93be4
jenberu <jemberu0970@gmail.com>
2025-06-18 15:09:59 +0300

    Refactor project structure: update .gitignore and modify Django settings path in manage.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+#user model
+class CustomUser(AbstractUser):
+    ROLES = (
+        ('ADMIN', 'Admin'),
+        ('HR', 'HR'),
+        ('EMPLOYEE', 'Employee'),
+    )
+    role = models.CharField(max_length=10, choices=ROLES, default='EMPLOYEE')
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+    
+    def is_admin(self):
+        return self.role == 'ADMIN'
+    
+    def is_hr(self):
+        return self.role == 'HR'
+    
+    def is_employee(self):
+        return self.role == 'EMPLOYEE'

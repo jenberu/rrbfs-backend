fd45c04bc58e5cd2d3f3e5296862fe2309745bfd
jenberu <jemberu0970@gmail.com>
2025-06-19 23:22:14 +0300

    Enhance DocumentSerializer with file URL generation and update fields; refactor URLs for document management

--- documents/serializers.py
+++ documents/serializers.py
@@ -1,8 +1,34 @@
 from rest_framework import serializers
 from .models import Document
+from accounts.serializers import UserSerializer,DepartmentSerializer
+from accounts.models import CustomUser, Department
 
 class DocumentSerializer(serializers.ModelSerializer):
+    uploaded_by = UserSerializer(read_only=True)
+    department = DepartmentSerializer(read_only=True)
+    department_id = serializers.PrimaryKeyRelatedField(
+    queryset=Department.objects.all(), source='department', write_only=True
+     )
+    uploaded_by_id = serializers.PrimaryKeyRelatedField(
+    queryset=CustomUser.objects.all(), source='uploaded_by', write_only=True
+       )
+    file_url = serializers.SerializerMethodField()
+    def get_file_url(self, obj):
+      if obj.file:
+            url = obj.file.url
+            filename = obj.name or 'file'
+            return url.replace('/upload/', f'/upload/fl_attachment:{filename}/')
+      return None
+
     class Meta:
         model = Document
-        fields = '__all__'
-        read_only_fields = ['uploaded_by', 'department', 'uploaded_at', 'updated_at']
+        fields = [
+            'id', 'name', 'description', 'category', 'file_url',
+            'uploaded_by', 'department',
+            'uploaded_by_id', 'department_id',
+            'uploaded_at', 'updated_at',
+        ]
+        
+
+
+
--- documents/urls.py
+++ documents/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import DocumentUploadView, DocumentListView, DocumentDetailView, DocumentDeleteView
+from .views import (DocumentUploadView, 
+                    DocumentListView,
+                     DocumentDetailView, 
+                     DocumentDeleteView,
+                     TotalDocumentsView)
 
 urlpatterns = [
     path('upload/', DocumentUploadView.as_view(), name='upload-document'),
     path('', DocumentListView.as_view(), name='list-documents'),
     path('<int:pk>/', DocumentDetailView.as_view(), name='document-detail'),
-    path('<int:pk>/delete/', DocumentDeleteView.as_view(), name='document-delete'),
+    path('delete/', DocumentDeleteView.as_view(), name='document-delete'),
+    path("total-documents/", TotalDocumentsView.as_view(), name="total-documents"),
+
 ]

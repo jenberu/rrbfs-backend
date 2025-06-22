404d995fc10b48f01e6fc39d20a1a92c9bc0c851
jenberu <jemberu0970@gmail.com>
2025-06-22 20:46:45 +0300

    Remove debug warnings and set DEBUG to True for development

--- config/settings.py
+++ config/settings.py
@@ -7,10 +7,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = config('DEBUG', default=False, cast=bool)
 SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
